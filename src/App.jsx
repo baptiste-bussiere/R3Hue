b2be0a2fb62e725d7a68a35d962942a67e82532e
baptiste buss <contact@baptiste-bussiere.fr>
2023-11-28 22:04:40 +0100

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import Scene from "./components/scene.jsx";
 import "./scss/style.scss";
 import { useFrame } from "@react-three/fiber";
-import Overlay from "./components/Overlay.jsx";
 import { AppContextProvider } from "./context/AppContext.jsx";
-
+import axios from "axios";
 function App() {
+  console.log(import.meta.env.VITE_USER_TOKEN);
   return (
     <>
       <AppContextProvider>
